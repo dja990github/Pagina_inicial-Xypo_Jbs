2f5d447486f9eb14a62a03f8aa79faa9e6b5463e
dja990github <144055934+dja990github@users.noreply.github.com>
2024-12-03 10:49:50 -0300

    Update temas.js

--- temas.js
+++ temas.js
@@ -1,16 +1,8 @@
-'use strict'
-
-const switcher = document.querySelector('.BtnTema');
-
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
-
-    var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Tema";
-}
-    else { 
-        this.textContent = "Tema";
-    }
-
-});
+        var vn = 2;
+        function temabtn() {
+            if (vn % 2 === 0) {
+                document.body.classList.toggle('dark-theme');  
+            } else {
+                document.body.classList.toggle('light-theme');  
+            }
+        }
