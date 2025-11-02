8be988e0796872b67ed879d0c7b672d899df264f
dja990github <144055934+dja990github@users.noreply.github.com>
2025-11-01 22:45:11 -0300

    Delete temas.js

--- temas.js
+++
@@ -1,8 +0,0 @@
-        var vn = 2;
-        function temabtn() {
-            if (vn % 2 === 0) {
-                document.body.classList.toggle('dark-theme');  
-            } else {
-                document.body.classList.toggle('light-theme');  
-            }
-        }
