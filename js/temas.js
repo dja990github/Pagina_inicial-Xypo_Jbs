d5cf508ba170500b0a80db4275a44f19bfa8ac8c
dja990github <144055934+dja990github@users.noreply.github.com>
2025-11-01 22:46:37 -0300

    Add files via upload

---
+++ js/temas.js
@@ -0,0 +1,8 @@
+        var vn = 2;
+        function temabtn() {
+            if (vn % 2 === 0) {
+                document.body.classList.toggle('dark-theme');  
+            } else {
+                document.body.classList.toggle('light-theme');  
+            }
+        }
